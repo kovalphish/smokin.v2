249c8a7c28e160bc7b8200d718ac64f07682f1e9
kovalphish <145151831+kovalphish@users.noreply.github.com>
2026-02-14 01:25:04 +0500

    getInvoice.js

--- api/getInvoice.js
+++ api/getInvoice.js
@@ -1,16 +1,17 @@
+// api/getInvoice.js
 const axios = require('axios');
 
 export default async function handler(req, res) {
     const { userId } = req.query;
-    const BOT_TOKEN = "8470694002:AAGYITMQ3yNIuoP3w7MqQmnmWmRF66kzUd0"; // Вставь токен от BotFather
+    const BOT_TOKEN = "8470694002:AAGYITMQ3yNIuoP3w7MqQmnmWmRF66kzUd0"; // <--- ВСТАВЬ ТОКЕН ОТ @BotFather
 
     try {
         const response = await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/createInvoiceLink`, {
-            title: "Спин в рулетке",
-            description: "1 попытка для Smokin174",
-            payload: userId, // Передаем ID, чтобы знать, кому дать спин
-            currency: "XTR",
-            prices: [{ label: "1 попытка", amount: 10 }] // 10 звезд
+            title: "LUCKY SMOKIN SPIN",
+            description: "1 попытка в рулетке",
+            payload: userId, 
+            currency: "XTR", // Валюта звезд
+            prices: [{ label: "Спин", amount: 10 }] // Цена: 10 звезд
         });
         res.status(200).json({ link: response.data.result });
     } catch (e) {
